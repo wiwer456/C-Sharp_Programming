ff44c3e0a1bfd4761c774d5bb2009d57a3922f8e
wiwer456 <67992786+wiwer456@users.noreply.github.com>
2025-03-21 13:13:51 +0300

    Update t10.cs

--- task10/t10.cs
+++ task10/t10.cs
@@ -2,9 +2,9 @@ Random rnd = new Random();
 double[] zp = new double[12];
 double zp_all = 0;
 double zp_pen = 0;
-double zp_max = int.MinValue;
+double zp_max = double.MinValue;
 double zp_max_num = 0;
-double zp_min = int.MaxValue;
+double zp_min = double.MaxValue;
 double zp_min_num = 0;
 double razn = 0;
 
@@ -23,13 +23,13 @@ for (int i = 0;i < zp.Length; i++)
     Console.ForegroundColor = ConsoleColor.DarkCyan;
     Console.Write($" (отчисление в фонд: {Math.Round(zp[i] * 0.02, 2)}$)");
 
-    razn = Math.Floor(Math.Abs((zp_all / 12) - zp[i]));
+    razn = Math.Round(Math.Abs((zp_all / 12) - zp[i]), 2);
     if (zp_all / 12 > zp[i]) { Console.ForegroundColor= ConsoleColor.Red; }
     else if (zp_all / 12 < zp[i]) { Console.ForegroundColor = ConsoleColor.Green; }
     Console.WriteLine($" | отклонение от средней з.п. - {razn}");
 
     zp_pen += zp[i] * 0.02;
-    if (zp[i] > zp_max) {  
+    if (zp[i] > zp_max) {
         zp_max = zp[i];
         zp_max_num = i + 1;
     }
