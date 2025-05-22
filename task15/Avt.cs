bac32524c5aff14d2a5b892c37bc92745852c79c
wiwer456 <67992786+wiwer456@users.noreply.github.com>
2025-05-22 10:56:55 +0300

    add Avto

---
+++ task15/Avt.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Avt
+{
+    private string Avto_number;
+    private string Avto_marka;
+    private double Fuel_value; // кол-во в баке
+    private double Fuel_rate; // расход
+    private double Probeg;
+    private double Max_fuel_value;
+    private int Pos_x = 0;
+    private int Pos_y = 0;
+    private int Speed;
+    public void Info(string nom, string marka, double bak, double max_bak, double ras)
+    {
+        Avto_number = nom;
+        Avto_marka = marka;
+        Fuel_value = bak;
+        Max_fuel_value = max_bak;
+        Fuel_rate = ras;
+        Probeg = 0;
+    }
+    public void Out()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"| Номер авто: {Avto_number}\n" +
+                 $"| Марка авто: {Avto_marka}\n" +
+                 $"| Кол-во бензина: {Fuel_value:F2} л\n" +  
+                 $"| Расход бензина: {Fuel_rate:F2} л/100км\n" +
+                 $"| Пробег: {Probeg:F2} км\n" +
+                 $"| Позиция: ({Pos_x}; {Pos_y})\n");
+        Console.ResetColor();
+    }
+    public void Zaprafka(double top)
+    {
+        if (Fuel_value + top <= Max_fuel_value)
+        {
+            Fuel_value += top;
+            Ostatok();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("!Слишком большое кол-во бензина, бак залит до максимума");
+            Console.ResetColor();
+            Fuel_value = Max_fuel_value;
+        }
+    }
+    public void Move(int x, int y)
+    {
+        double km = Math.Sqrt(Math.Pow(x - Pos_x, 2) + Math.Pow(y - Pos_y, 2));
+        Console.WriteLine($"Нужно проехать {km:F2} км");
+        Razgon();
+        Console.WriteLine($"Машина едет со скоростью {Speed} км/ч");
+        double tmp_km = km;
+        while (km != 0)
+        {
+            if ((Fuel_value / Fuel_rate) * 100 >= km)
+            {
+                Fuel_value -= (km / 100) * Fuel_rate;
+                km = 0;
+                Probeg += tmp_km;
+                Pos_x = x;
+                Pos_y = y;
+                Stop();
+                Ostatok();
+            }
+            else if((Fuel_value / Fuel_rate) * 100 < km)
+            {
+                km -= (Fuel_value / Fuel_rate) * 100;
+                Probeg += (Fuel_value / Fuel_rate) * 100;
+                Fuel_value = 0;
+                while (true)
+                {
+                    Console.WriteLine($"Закончилось топливо, осталось проехать {km:F2}км за {((Fuel_rate * km) / 100):F2}л. Сделать остановку для заправки?\n1) Да\n2) Нет");
+                    if (int.TryParse(Console.ReadLine(), out int u_answer))
+                    {
+                        if (u_answer == 1)
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("Какое кол-во топлива заливаем?");
+                                if (double.TryParse(Console.ReadLine(), out double f_value) && f_value > 0)
+                                {
+                                    Zaprafka(f_value);
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("!Некорректное число");
+                                }
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Stop();
+                            km = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public void Ostatok()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Остаток топлива: {Fuel_value:F2}");
+        Console.ResetColor();
+    }
+    public void Stop()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Машина остановилась");
+        Console.ResetColor();
+    }
+    public void Razgon()
+    {
+        Random rnd = new Random();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Машина разгоняется...");
+        Console.ResetColor();
+        Speed = rnd.Next(30, 100);
+    }
+    public void Avaria(Avt car)
+    {
+        Console.WriteLine($"Машина под номером {Avto_number} врезалось в магину под номером {car.Avto_number}");
+    }
+    public Avt(string avto_number = "", string avto_marka = "", double fuel_value = 0, double fuel_rate = 0, double probeg = 0, double max_fuel_value = 0)
+    {
+        Avto_number = avto_number;
+        Avto_marka = avto_marka;
+        Fuel_value = fuel_value;
+        Fuel_rate = fuel_rate;
+        Probeg = probeg;
+        Max_fuel_value = max_fuel_value;
+    }
+
+}
---
+++ task15/Program.cs
@@ -0,0 +1,206 @@
+﻿
+class Program()
+{
+    static void Main()
+    {
+        int cars_count = IntChecker("Введите размер автопарка: "); ;
+        while (cars_count < 1)
+        {
+            Print("Введите число от 1");
+            cars_count = IntChecker("Введите размер автопарка: ");
+        }
+        Random rnd = new Random();
+
+        Avt[] cars = new Avt[cars_count];
+        int id = 0;
+        int u_id = 0;
+        Console.Clear();
+        while (true)
+        {
+            int u_answer = IntChecker("Меню:\n1) Добавить машину\n" +
+                "2) Выбрать машину\n" +
+                "3) Ехать на машине\n" +
+                "4) Заправить машину\n" +
+                "5) Вывести информацию о машинах\n" +
+                "6) Авария\n");
+            while (u_answer > 6 || u_answer < 1) 
+            {
+                Print("Введите число от 1 до 6");
+                u_answer = IntChecker("Меню:\n1) Добавить машину\n" +
+                "2) Выбрать машину\n" +
+                "3) Ехать на машине\n" +
+                "4) Заправить машину\n" +
+                "5) Вывести информацию о машинах\n" +
+                "6) Авария\n");
+            }
+            switch (u_answer)
+            {
+                case 1:
+                    Console.Clear();
+                    if (id > cars.Length-1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("!Достигнут максимум по кол-ву машин");
+                        Console.ResetColor();
+                        break;
+                    }
+                    
+                    Console.Write("Введеите номер машины: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Введеите марку машины: ");
+                    string marka = Console.ReadLine();
+                    double max_fuel = doubleChecker("Объём бака: "); ;
+                    while (max_fuel <= 0)
+                    {
+                        Print("Введите число больше 0");
+                        max_fuel = doubleChecker("Объём бака: ");
+                    }
+                    double bak = doubleChecker("Начальное кол-во бензина: "); ;
+                    while (bak > max_fuel || bak <= 0)
+                    {
+                        Print($"Введите число от 0 до {max_fuel}");
+                        bak = doubleChecker("Начальное кол-во бензина: ");
+                    }
+                    double ras = doubleChecker("Расход бензина на 100км: "); ;
+                    while (ras <= 0)
+                    {
+                        Print("Введите число больше 0");
+                        ras = doubleChecker("Расход бензина на 100км: ");
+                    }
+                    Console.Clear();
+                    Avt car = new Avt();
+                    car.Info(name, marka, bak, max_fuel, ras);
+                    cars[id] = car;
+                    id++;
+                    break;
+                case 2:
+                    Console.Clear();
+                    if (id < 1) { goto case 7; }
+                    u_id = IntChecker("Введите id машины: ");
+                    while(u_id < 0 || u_id >= cars_count)
+                    {
+                        Print("Машина с таким id не существует");
+                        u_id = IntChecker("Введите id машины: ");
+                    }
+                    break;
+                case 3:
+                    if (cars[u_id] != null)
+                    {
+                        int x = IntChecker("Введите новую координату x: ");
+                        int y = IntChecker("Введите новую координату y: ");
+                        Console.Clear();
+                        cars[u_id].Move(x, y);
+                        break;
+                    }
+                    goto case 7;
+                case 4:
+                    Console.Clear();
+                    if (cars[u_id] != null && cars[0] != null)
+                    {
+                        double tmp_fu = doubleChecker("Кол-во топлива: ");
+                        while (tmp_fu <= 0)
+                        {
+                            Print("Введите число больше 0");
+                            tmp_fu = doubleChecker("Кол-во топлива: ");
+                        }
+                        cars[u_id].Zaprafka(tmp_fu);
+                        break;
+                    }
+                    goto case 7;
+                case 5:
+                    if (cars[0] != null)
+                    {
+                        int u_ans = IntChecker("1) Вывести информацию о выбранной машине\n" +
+                            "2) Вывести информацию о всех машинах\n");
+                        while (u_ans < 1 || u_ans > 2)
+                        {
+                            Print("Введите число от 1 до 2");
+                            u_ans = IntChecker("1) Вывести информацию о выбранной машине\n" +
+                            "2) Вывести информацию о всех машинах\n");
+                        }
+                        Console.Clear();
+                        Console.WriteLine("Информация:\n");
+                        switch (u_ans)
+                        {
+                            case 1:
+                                cars[u_id].Out();
+                                break;
+                            case 2:
+                                foreach (Avt av in cars)
+                                {
+                                    if (av != null) { av.Out(); }
+                                }
+                                break;
+                        }
+                        break;
+                    }
+                    goto case 7;
+                case 6:
+                    Console.Clear();
+                    if (cars[0] != null)
+                    {
+                        int car_c = rnd.Next(0, cars_count);
+                        if (car_c != u_id && cars[car_c] != null)
+                        {
+                            cars[u_id].Avaria(cars[car_c]);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Машина врезалась в забор");
+                            break;
+                        }
+                    }
+                    goto case 7;
+                case 7:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("!Заполните информацию о машинах");
+                    Console.ResetColor();
+                    break;
+
+            }
+        }
+    }
+    static double doubleChecker(string text)
+    {
+        while (true)
+        {
+            Console.Write(text);
+            if (double.TryParse(Console.ReadLine(), out double u_answer))
+            {
+                return u_answer;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("!Неверный формат");
+                Console.ResetColor();
+            }
+        }
+    }
+    static int IntChecker(string text)
+    {
+        while (true)
+        {
+            Console.Write(text);
+            if (int.TryParse(Console.ReadLine(), out int u_answer))
+            {
+                return u_answer;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("!Неверный формат");
+                Console.ResetColor();
+            }
+        }
+    }
+    static void Print(string str)
+    {
+        Console.Clear();
+        Console.ForegroundColor= ConsoleColor.Red;
+        Console.WriteLine(str);
+        Console.ResetColor();
+    }
+}
